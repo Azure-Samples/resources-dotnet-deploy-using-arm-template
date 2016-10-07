b58cc66ecf9dd6671a5ef4cd1724d38f82d1ed65
Anudeep Sharma <anudeepsharma@users.noreply.github.com>
2016-10-06 17:59:01 -0700

    Updated sample (#2)

---
+++ Program.cs
@@ -0,0 +1,112 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License. See License.txt in the project root for license information.
+
+using Microsoft.Azure.Management.Fluent;
+using Microsoft.Azure.Management.Resource.Fluent;
+using Microsoft.Azure.Management.Resource.Fluent.Authentication;
+using Microsoft.Azure.Management.Resource.Fluent.Core;
+using Newtonsoft.Json.Linq;
+using System;
+
+namespace DeployUsingARMTemplate
+{
+    /**
+     * Azure Resource sample for deploying resources using an ARM template.
+     */
+
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            try
+            {
+                var rgName = ResourceNamer.RandomResourceName("rgRSAT", 24);
+                var deploymentName = ResourceNamer.RandomResourceName("dpRSAT", 24);
+
+                try
+                {
+                    //=================================================================
+                    // Authenticate
+                    var credentials = AzureCredentials.FromFile(Environment.GetEnvironmentVariable("AZURE_AUTH_LOCATION"));
+
+                    var azure = Azure
+                        .Configure()
+                        .WithLogLevel(HttpLoggingDelegatingHandler.Level.BASIC)
+                        .Authenticate(credentials)
+                        .WithDefaultSubscription();
+
+                    try
+                    {
+                        var templateJson = GetTemplate();
+
+                        //=============================================================
+                        // Create resource group.
+
+                        Console.WriteLine("Creating a resource group with name: " + rgName);
+
+                        azure.ResourceGroups.Define(rgName)
+                            .WithRegion(Region.US_WEST)
+                            .Create();
+
+                        Console.WriteLine("Created a resource group with name: " + rgName);
+
+                        //=============================================================
+                        // Create a deployment for an Azure App Service via an ARM
+                        // template.
+
+                        Console.WriteLine("Starting a deployment for an Azure App Service: " + deploymentName);
+
+                        azure.Deployments.Define(deploymentName)
+                            .WithExistingResourceGroup(rgName)
+                            .WithTemplate(templateJson)
+                            .WithParameters("{}")
+                            .WithMode(Microsoft.Azure.Management.Resource.Fluent.Models.DeploymentMode.Incremental)
+                            .Create();
+
+                        Console.WriteLine("Completed the deployment: " + deploymentName);
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine(ex);
+                    }
+                    finally
+                    {
+                        try
+                        {
+                            Console.WriteLine("Deleting Resource Group: " + rgName);
+                            azure.ResourceGroups.Delete(rgName);
+                            Console.WriteLine("Deleted Resource Group: " + rgName);
+                        }
+                        catch (Exception ex)
+                        {
+                            Console.WriteLine(ex);
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
+        }
+
+        private static string GetTemplate()
+        {
+            var hostingPlanName = ResourceNamer.RandomResourceName("hpRSAT", 24);
+            var webAppName = ResourceNamer.RandomResourceName("wnRSAT", 24);
+            var armTemplateString = System.IO.File.ReadAllText(@".\ARMTemplate\TemplateValue.json");
+
+            var parsedTemplate = JObject.Parse(armTemplateString);
+            parsedTemplate.SelectToken("parameters.hostingPlanName")["defaultValue"] = hostingPlanName;
+            parsedTemplate.SelectToken("parameters.webSiteName")["defaultValue"] = webAppName;
+            parsedTemplate.SelectToken("parameters.skuName")["defaultValue"] = "F1";
+            parsedTemplate.SelectToken("parameters.skuCapacity")["defaultValue"] = 1;
+
+            return parsedTemplate.ToString();
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,34 @@
-# resources-dotnet-deploy-using-arm-template
+---
+services: Resources
+platforms: .Net
+author: alvadb
+---
+
+#Getting Started with Resources - Deploy Using A R M Template - in .Net #
+
+      Azure Resource sample for deploying resources using an ARM template.
+
+
+## Running this Sample ##
+
+To run this sample:
+
+Set the environment variable `AZURE_AUTH_LOCATION` with the full path for an auth file. See [how to create an auth file](https://github.com/Azure/azure-sdk-for-net/blob/Fluent/AUTH.md).
+
+    git clone https://github.com/Azure-Samples/resources-dotnet-deploy-using-arm-template.git
+
+    cd resources-dotnet-deploy-using-arm-template
+
+    dotnet restore
+
+    dotnet run
+
+## More information ##
+
+[Azure Management Libraries for C#](https://github.com/Azure/azure-sdk-for-net/tree/Fluent)
+[Azure .Net Developer Center](https://azure.microsoft.com/en-us/develop/net/)
+If you don't have a Microsoft Azure subscription you can get a FREE trial account [here](http://go.microsoft.com/fwlink/?LinkId=330212)
+
+---
+
+This project has adopted the [Microsoft Open Source Code of Conduct](https://opensource.microsoft.com/codeofconduct/). For more information see the [Code of Conduct FAQ](https://opensource.microsoft.com/codeofconduct/faq/) or contact [opencode@microsoft.com](mailto:opencode@microsoft.com) with any additional questions or comments.
